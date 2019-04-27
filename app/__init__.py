62ecac4613f9c577983c1410a92b6d774f7ca9a5
Harvey Chan <ch4ch4@gmail.com>
2019-04-27 15:08:41 -0700

    add sample code for fetching user from mongodb

--- app/__init__.py
+++ app/__init__.py
@@ -3,15 +3,18 @@ import os
 import requests
 from flask import Flask, render_template, request, redirect, url_for
 from flask_pymongo import PyMongo
+from pymongo import MongoClient
 
 app = Flask(__name__)
 MONGODB_URI = os.getenv('MONGODB_URI', "")
-app.config["MONGO_URI"] = MONGODB_URI
-mongo = PyMongo(app)
+client = MongoClient(MONGODB_URI)
+db = client['heroku_q29ztbtc']
+
+online_users = db.users.find_one()
+print(str(online_users))
 
 @app.route('/')
 def index():
-	online_users = mongo.db.users.find()
-	print(online_users)
+	print(str(online_users))
 
 	return render_template("index.html")
