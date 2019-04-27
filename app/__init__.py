6099db0d67c927503ae720bef35c834cf111a1dc
C.R.A.S.H <mardirossian.sevan@gmail.com>
2019-04-27 14:49:24 -0700

    working code

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,6 @@ import requests
 from flask import Flask, render_template, request, redirect, url_for
 from flask_pymongo import PyMongo
 
-
 app = Flask(__name__)
 MONGODB_URI = os.getenv('MONGODB_URI', "")
 app.config["MONGO_URI"] = MONGODB_URI
