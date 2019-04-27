63c39a2be35db15fdf954ed30eaadead9c2ca898
Harvey Chan <ch4ch4@gmail.com>
2019-04-27 14:30:15 -0700

    add mongo connection string

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,17 @@ import sys
 import os
 import requests
 from flask import Flask, render_template, request, redirect, url_for
+from flask_pymongo import PyMongo
+
 
 app = Flask(__name__)
+MONGODB_URI = os.getenv('MONGODB_URI', "")
+app.config["MONGO_URI"] = MONGODB_URI
+mongo = PyMongo(app)
 
 @app.route('/')
 def index():
-	
+    online_users = mongo.db.users.find()
+	print(online_users)
+
 	return render_template("index.html")
