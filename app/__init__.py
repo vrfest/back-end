a9f0b7cc4ca3cbc2ab1bfdcc1dd138508a636d68
Harvey Chan <ch4ch4@gmail.com>
2019-04-27 15:11:13 -0700

    forgot to save

--- app/__init__.py
+++ app/__init__.py
@@ -10,11 +10,9 @@ MONGODB_URI = os.getenv('MONGODB_URI', "")
 client = MongoClient(MONGODB_URI)
 db = client['heroku_q29ztbtc']
 
-online_users = db.users.find_one()
-print(str(online_users))
-
 @app.route('/')
 def index():
+	online_users = db.users.find_one()
 	print(str(online_users))
 
 	return render_template("index.html")
