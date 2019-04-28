6379ed281d730e59bd12842dc820a2505099a77a
Harvey Chan <ch4ch4@gmail.com>
2019-04-27 18:04:10 -0700

    add compiled frontend stuff

--- app/__init__.py
+++ app/__init__.py
@@ -4,17 +4,14 @@ from pymongo import MongoClient
 
 from app.ticket import TicketController
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 MONGODB_URI = os.getenv('MONGODB_URI', "")
 client = MongoClient(MONGODB_URI)
 db = client['heroku_q29ztbtc']
 
 @app.route('/')
 def index():
-	online_users = db.users.find_one()
-	print(str(online_users))
-
-	return render_template("index.html")
+	return app.send_static_file("index.html")
 
 # @app.route('/tickets/create', methods=['POST'])
 # def buy_ticket():
---
+++ app/static/precache-manifest.b261d8136b22079bf921c17ac4fbd73b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "c39410068076da734b5280593eabf585",
+    "url": "/static/media/vrfest.c3941006.png"
+  },
+  {
+    "revision": "ba05b741591b4c83924b",
+    "url": "/static/css/main.399e3c15.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "356a26c2adb2f9ed5fd7",
+    "url": "/static/js/2.556c7599.chunk.js"
+  },
+  {
+    "revision": "822649002f97dae76b28e9b8f5f998ce",
+    "url": "/static/media/logo-black.82264900.png"
+  },
+  {
+    "revision": "0104b21a51ba9b4ab9f310b37999b6ec",
+    "url": "/static/media/splash.0104b21a.mp4"
+  },
+  {
+    "revision": "ba05b741591b4c83924b",
+    "url": "/static/js/main.81c4a1be.chunk.js"
+  },
+  {
+    "revision": "64406ec22c428e45c61711096fa81930",
+    "url": "/static/media/card.64406ec2.png"
+  },
+  {
+    "revision": "29d9951effd314ef6aa1639af9362f9f",
+    "url": "/static/media/landing-logo.29d9951e.png"
+  },
+  {
+    "revision": "b3f08d05ac213c595d7feb26767ddd0c",
+    "url": "/static/media/background.b3f08d05.png"
+  },
+  {
+    "revision": "62ae3eec77e7d516c3c15427e93e6f07",
+    "url": "/static/media/landing-background.62ae3eec.png"
+  },
+  {
+    "revision": "3500bfde5fa7c9d90cdb97b05108e183",
+    "url": "/index.html"
+  }
+];
